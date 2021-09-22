38eb9a44824a35dff13c2b530466d6badee46f04
b2jena <b2j1999jan27@gmail.com>
2021-09-22 11:39:34 +0530

    added add-resource component

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -3,19 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMedicineComponent } from './components/add-medicine/add-medicine.component';
+import { ListMedicinesComponent } from './components/list-medicines/list-medicines.component';
 import { SosRequestComponent } from './components/sos-request/sos-request.component';
-
+import { UpdateMedicineComponent } from './components/update-medicine/update-medicine.component';
+import {FormsModule} from '@angular/forms';
+import { SMedicineService } from './services/s-medicine.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SosRequestComponent
+    SosRequestComponent,
+    UpdateMedicineComponent,
+    ListMedicinesComponent,
+    AddMedicineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,FormsModule
   ],
-  providers: [],
+  providers: [HttpClient,SMedicineService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
