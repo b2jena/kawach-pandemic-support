e793a11fd208672b79e72524b9be8195caf05030
Ubuntu <ubuntu@ip-172-31-35-73.ap-south-1.compute.internal>
2021-09-23 08:21:38 +0000

    added address, contactPerson, mobileNumber

--- product-webapp/webapp/src/app/services/s-medicine.service.ts
+++ product-webapp/webapp/src/app/services/s-medicine.service.ts
@@ -5,38 +5,37 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class SMedicineService {
-  
+export class SMedicineService{
   Medicine: Medicine[] = [];
-  
   constructor(private httpService: HttpClient) { }
-
-  CreateMedicine(medicine: Medicine):Observable<Medicine> {
+  CreateMedicine(medicine: Medicine): Observable<Medicine> {
     console.log(medicine);
-      return this.httpService.post<Medicine>("http://localhost:9999/medicine/create",medicine);
+    return this.httpService.post<Medicine>('http://localhost:9999/medicine/create', medicine);
   }
   public deleteMedicine(medicine: Medicine) {
-    return this.httpService.delete<Medicine>("http://localhost:9999/medicine//delete/"+ medicine.city);
+    return this.httpService.delete<Medicine>('http://localhost:9999/medicine//delete/' + medicine.city);
   }
   public updateMedicine(medicine: Medicine) {
     console.log(medicine);
-    return this.httpService.put<Medicine>("http://localhost:9999/medicine/update", medicine);
+    return this.httpService.put<Medicine>('http://localhost:9999/medicine/update', medicine);
   }
   getMedicines(){
-    return this.httpService.get<Medicine[]>("http://localhost:9999/medicine/getAll")
+    return this.httpService.get<Medicine[]>('http://localhost:9999/medicine/getAll');
   }
 }
-
 export class Medicine{
-
-  public city:string;
-  public medName:string;
-  public pharmacy:string;
-
-  constructor( city:string,medName:string,pharmacy:string) {
-    this.city=city;
-    this.medName=medName;
-    this.pharmacy=pharmacy
-
+  public city: string;
+  public medName: string;
+  public pharmacy: string;
+  public address: string;
+  public contactPerson: string;
+  public mobileNumber: string;
+  constructor( city: string, medName: string, pharmacy: string, address: string, contactPerson: string, mobileNumber: string) {
+    this.city = city;
+    this.medName = medName;
+    this.pharmacy = pharmacy;
+    this.address = address;
+    this.contactPerson = contactPerson;
+    this.mobileNumber = mobileNumber;
   }
 }
