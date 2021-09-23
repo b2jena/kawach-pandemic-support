5092d313fe658dc009337c5d3c8df5fc779a3c38
godwinkhalko2 <godwinkhalko2@gmail.com>
2021-09-23 10:19:09 +0000

    Finished the patient-otp-service

--- patient-otp-service/src/main/java/com/stackroute/repository/PatientRepo.java
+++ patient-otp-service/src/main/java/com/stackroute/repository/PatientRepo.java
@@ -4,5 +4,9 @@ package com.stackroute.repository;
 import com.stackroute.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PatientRepo extends JpaRepository<Patient, String> {
+    Patient save(Patient patient);
+    List<Patient> getAll();
 }
---
+++ patient-otp-service/src/main/java/com/stackroute/service/PatientServiceI.java
@@ -0,0 +1,10 @@
+package com.stackroute.service;
+
+import com.stackroute.entity.Patient;
+
+import java.util.List;
+
+public interface PatientServiceI {
+    Patient saveUser(Patient patient);
+    List<Patient> getAll();
+}
