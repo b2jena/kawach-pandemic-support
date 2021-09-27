384175b49624f83ef5f4b47c416f86addd52f34e
sourya777 <sourya777@gmail.com>
2021-09-27 14:58:36 +0530

    login error handler

--- product-webapp/webapp/src/app/services/login.service.ts
+++ product-webapp/webapp/src/app/services/login.service.ts
@@ -21,11 +21,14 @@ export class LoginService {
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
+      alert('An error occurred:' + error.error)
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
+        alert(
+          `Error code ${error.status}:` + error.error);
     }
     // Return an observable with a user-facing error message.
     return throwError(
