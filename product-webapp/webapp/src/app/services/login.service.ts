853a6db1c54e3bf5db8c74b564a050407e84a2e7
sourya777 <sourya777@gmail.com>
2021-09-27 12:08:23 +0530

    login error handling

--- product-webapp/webapp/src/app/services/login.service.ts
+++ product-webapp/webapp/src/app/services/login.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,23 @@ export class LoginService {
 
   public generateToken(user: User): Observable<string[]>{
     const jsonstr: string = '{ "id":"' + user.id + '", "password":"' + user.password + '" }';
-    return this.http.post<string[]>('http://localhost:9099/api/v1/login/user', JSON.parse(jsonstr));
+    return this.http.post<string[]>('http://localhost:9099/api/v1/login/user', JSON.parse(jsonstr)).pipe(
+      catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 
 }
