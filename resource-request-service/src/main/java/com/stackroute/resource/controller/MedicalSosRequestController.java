3bbfc42b05108f04e778a927d3d5d6a591d9f6ce
rajbullu1998 <rajbullu1998@gmail.com>
2021-09-28 04:26:15 +0000

    Added @put for SosRequest updation

--- resource-request-service/src/main/java/com/stackroute/resource/controller/MedicalSosRequestController.java
+++ resource-request-service/src/main/java/com/stackroute/resource/controller/MedicalSosRequestController.java
@@ -7,17 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
 public class MedicalSosRequestController {
     private MedicalSosRequestService medicalSosRequestService;
-
     @Autowired
     public MedicalSosRequestController(MedicalSosRequestService medicalSosRequestService){
         this.medicalSosRequestService = medicalSosRequestService;
     }
-
     @PostMapping("sos/createSos")
     public ResponseEntity<MedicalSosRequest> saveSosRequest(@RequestBody MedicalSosRequest medicalSosRequest){
         MedicalSosRequest savedSosRequest = medicalSosRequestService.saveSosRequest(medicalSosRequest);
@@ -27,5 +26,11 @@ public class MedicalSosRequestController {
     @GetMapping("sos/getSos")
     public ResponseEntity<List<MedicalSosRequest>> getSosRequest(){
         return new ResponseEntity<List<MedicalSosRequest>>((List<MedicalSosRequest>)medicalSosRequestService.getSosRequest(),HttpStatus.OK);
+    }
+
+    @PutMapping(path="sos/statusSos/{requestId}")
+    public ResponseEntity<String> updateSosRequest(@PathVariable("requestId") UUID requestId){
+        medicalSosRequestService.updateSosRequest(requestId);
+        return new ResponseEntity<String>("updated successfully" ,HttpStatus.OK );
     }
 }
--- resource-request-service/src/main/java/com/stackroute/resource/repository/MedicalSosRequestRepository.java
+++ resource-request-service/src/main/java/com/stackroute/resource/repository/MedicalSosRequestRepository.java
@@ -2,8 +2,10 @@ package com.stackroute.resource.repository;
 
 import com.stackroute.resource.model.MedicalSosRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface MedicalSosRequestRepository extends MongoRepository<MedicalSosRequest, UUID> {
 }
