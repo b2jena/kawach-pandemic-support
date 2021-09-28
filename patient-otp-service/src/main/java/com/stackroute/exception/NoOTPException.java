d887138c39699984adb71ec9e1e5888d01162173
godwinkhalko2 <godwinkhalko2@gmail.com>
2021-09-28 06:48:27 +0000

    added redirect after otp

---
+++ patient-otp-service/src/main/java/com/stackroute/exception/NoOTPException.java
@@ -0,0 +1,7 @@
+package com.stackroute.exception;
+
+public class NoOTPException extends Exception{
+    public NoOTPException(String message) {super(message); }
+    public NoOTPException() {
+    }
+}
