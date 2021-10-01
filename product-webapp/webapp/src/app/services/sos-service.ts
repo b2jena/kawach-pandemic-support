5f0b8616fe8a50b272ddd16ba5db3030c7058cff
Ubuntu <ubuntu@ip-172-31-44-215.ap-south-1.compute.internal>
2021-10-01 11:06:55 +0000

    bug fixes in frontend

--- product-webapp/webapp/src/app/services/sos-service.ts
+++ product-webapp/webapp/src/app/services/sos-service.ts
@@ -13,7 +13,7 @@ export class SosService {
 
   CreateSosRequest(SosRequest: SOSRequest): Observable <SOSRequest> {
     console.log(SosRequest);
-    console.log(Requirment);
+    console.log(Requirement);
     return this.httpService.post<SOSRequest>('http://localhost:9901/sos/createSos', SosRequest);
   }
 
@@ -25,7 +25,7 @@ export class SosService {
 
 }
 
-export class Requirment{
+export class Requirement{
   public requirementName: string;
   public requirementQuantity: string;
   public unitOfMeasure: string;
@@ -45,19 +45,19 @@ export class SOSRequest{
   public email: string;
   public hospitalised: string;
   public city: string;
-  public requir: Array<Requirment>;
+  public requirement: Array<Requirement>;
   public requestStatus: string;
 
 
 
-  constructor( patientName: string, email: string, requir: Array <Requirment>, phoneNo: string, city: string, gender: string, hospitalised: string, requestStatus: string) {
+  constructor( patientName: string, email: string, requirement: Array <Requirement>, phoneNo: string, city: string, gender: string, hospitalised: string, requestStatus: string) {
     this.patientName = patientName;
     this.gender = gender;
     this.phoneNo = phoneNo;
     this.email = email;
     this.hospitalised = hospitalised;
     this.city = city;
-    this.requir = requir;
+    this.requirement = requirement;
     this.requestStatus = requestStatus;
   }
 }
