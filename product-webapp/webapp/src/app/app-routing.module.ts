d613a1f2c9c77b23f325af7205bc2dbf44d83ea3
Ubuntu <ubuntu@ip-172-31-44-215.ap-south-1.compute.internal>
2021-10-06 04:57:22 +0000

    video conferencing component

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ import { WarroomAddComponent } from './components/warroom-dashboard/warroom-add/
 // import { CloseSosRequestComponent } from './components/close-sos-request/close-sos-request.component';
 import { CloseSosComponent } from './components/close-sos/close-sos.component';
 import { HeaderTabsComponent } from './components/header-tabs/header-tabs.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 const routes: Routes = [
   {
@@ -76,6 +77,9 @@ const routes: Routes = [
   }, {
     path: 'closesos',
     component: CloseSosComponent
+  }, {
+    path: 'chat',
+    component: ChatComponent
   }
 ];
 
