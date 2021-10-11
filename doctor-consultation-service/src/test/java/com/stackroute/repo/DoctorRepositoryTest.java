d30b16100005895e290d2e15fdac4c4df4686f6b
godwinkhalko2 <godwinkhalko2@gmail.com>
2021-10-11 10:58:46 +0000

    Changed test cases

--- doctor-consultation-service/src/test/java/com/stackroute/repo/DoctorRepositoryTest.java
+++ doctor-consultation-service/src/test/java/com/stackroute/repo/DoctorRepositoryTest.java
@@ -3,16 +3,16 @@ package com.stackroute.repo;
 import com.stackroute.model.Doctor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 @DataMongoTest
 class DoctorRepositoryTest {
 
