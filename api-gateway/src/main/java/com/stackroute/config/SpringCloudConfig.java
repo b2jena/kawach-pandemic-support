57ecf89ccc666bb9b55a0b5f88ce8b615be1adf7
Piyush-Sourav <piyush98sourav@gmail.com>
2021-10-11 09:46:49 +0530

    Updated Spring Cloud Gateway

--- api-gateway/src/main/java/com/stackroute/config/SpringCloudConfig.java
+++ api-gateway/src/main/java/com/stackroute/config/SpringCloudConfig.java
@@ -1,4 +1,4 @@
-package com.stackroute.apigateway;
+package com.stackroute.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
