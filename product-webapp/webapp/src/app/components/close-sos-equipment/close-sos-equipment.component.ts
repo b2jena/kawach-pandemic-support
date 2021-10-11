5d7ee4b23bfa1006d2937432b96ae66ca4539fd4
rajbullu1998 <rajbullu1998@gmail.com>
2021-10-11 03:55:39 +0000

    Fetch SOS by type frontend integration for Bed and Equipment

--- product-webapp/webapp/src/app/components/close-sos-equipment/close-sos-equipment.component.ts
+++ product-webapp/webapp/src/app/components/close-sos-equipment/close-sos-equipment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CloseSos, CloseSosService, Requirement } from 'src/app/services/close-sos.service';
+
 
 @Component({
   selector: 'app-close-sos-equipment',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./close-sos-equipment.component.css']
 })
 export class CloseSosEquipmentComponent implements OnInit {
+  closeSos !: CloseSos[];
+  constructor(private closeSosService: CloseSosService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(){
+    this.closeSosService.getEquipSOS().subscribe(
+      response => this.handleSuccessfulResponse(response),
+    );
   }
-
+  handleSuccessfulResponse(response: CloseSos[]){
+    this.closeSos = response;
+  }
 }
