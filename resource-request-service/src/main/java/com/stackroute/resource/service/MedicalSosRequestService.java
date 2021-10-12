9d6c454a47c9d66d6ecd453e05999d31d489e5e8
godwinkhalko2 <godwinkhalko2@gmail.com>
2021-10-12 11:17:19 +0000

    Taking latest pull

--- resource-request-service/src/main/java/com/stackroute/resource/service/MedicalSosRequestService.java
+++ resource-request-service/src/main/java/com/stackroute/resource/service/MedicalSosRequestService.java
@@ -2,8 +2,6 @@ package com.stackroute.resource.service;
 
 import com.stackroute.resource.exception.NullValueException;
 import com.stackroute.resource.model.MedicalSosRequest;
-import com.stackroute.resource.model.Resources;
-//import com.stackroute.resource.model.Resources;
 
 import java.util.List;
 import java.util.UUID;
--- resource-request-service/src/main/java/com/stackroute/resource/service/MedicalSosRequestServiceImpl.java
+++ resource-request-service/src/main/java/com/stackroute/resource/service/MedicalSosRequestServiceImpl.java
@@ -2,7 +2,6 @@ package com.stackroute.resource.service;
 
 import com.stackroute.resource.exception.NullValueException;
 import com.stackroute.resource.model.MedicalSosRequest;
-import com.stackroute.resource.model.Resources;
 import com.stackroute.resource.repository.MedicalSosRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
