404905485e1ff773e1d58a7f57e5f72434ad9082
Bikash Jena <b2j1999jan27@gmail.com>
2021-10-13 17:54:04 +0530

    resource test cases

---
+++ resource-request-service/src/test/java/com/stackroute/resource/controller/EquipmentControllerTest.java
@@ -0,0 +1,101 @@
+package com.stackroute.resource.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stackroute.resource.model.Equipments;
+import com.stackroute.resource.service.EquipmentService;
+import com.stackroute.resource.service.EquipmentServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class EquipmentControllerTest {
+    private MockMvc mockMvc;
+    @Mock
+    EquipmentService equipmentService;
+    @InjectMocks
+    private EquipmentController equipmentController;
+
+    private Equipments equipments;
+    private List<Equipments> equipmentsList;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(equipmentController).build();
+        equipments = new Equipments();
+        equipments.setEquipmentId(UUID.randomUUID());
+        equipments.setEquipmentName("DemoEquipment");
+        equipments.setCity("DemoCity");
+        equipments.setHospital("DemoHospital");
+        equipments.setAddress("SampleAddressforTesting");
+        equipments.setContactPerson("Bikash Jena");
+        equipments.setMobileNumber("8327750692");
+        equipments.setVerificationStatus(true);
+        equipmentsList = new ArrayList<>();
+        equipmentsList.add(equipments);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        equipments = null;
+    }
+
+    @Test
+    void givenEquipmentToSaveThenShouldReturnSavedEquipment() throws Exception {
+        when(equipmentService.saveEquipment(any())).thenReturn(equipments);
+        mockMvc.perform(post("/equipment/create")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(asJsonString(equipments)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+        verify(equipmentService).saveEquipment(any());
+    }
+
+    @Test
+    public void givenGetAllEquipmentsThenShouldReturnListOfAllEquipments() throws Exception {
+        when(equipmentService.getAllEquipments()).thenReturn(equipmentsList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/equipment/getAll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(equipments)))
+                .andDo(MockMvcResultHandlers.print());
+        verify(equipmentService).getAllEquipments();
+        verify(equipmentService, times(1)).getAllEquipments();
+    }
+
+    @Test
+    void getUnverifiedBed() {
+    }
+
+    @Test
+    void verifyEquipment() {
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ resource-request-service/src/test/java/com/stackroute/resource/controller/ResourceControllerTest.java
@@ -0,0 +1,94 @@
+package com.stackroute.resource.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stackroute.resource.model.Resources;
+import com.stackroute.resource.model.Resources;
+import com.stackroute.resource.service.ResourceService;
+import com.stackroute.resource.service.ResourceServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class ResourceControllerTest {
+    private MockMvc mockMvc;
+    @Mock
+    ResourceService resourceService;
+    @InjectMocks
+    ResourceController resourceController;
+
+    private Resources resources;
+    private List<Resources> resourcesList;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(resourceController).build();
+        resources = new Resources();
+        resources.setId(UUID.randomUUID());
+        resources.setMedicineName("DemoMedicine");
+        resources.setCity("DemoCity");
+        resources.setPharmacy("DemoPharmacy");
+        resources.setAddress("SampleAddressforTesting");
+        resources.setContactPerson("Bikash Jena");
+        resources.setMobileNumber("8327750692");
+        resources.setVerificationStatus(true);
+        resourcesList = new ArrayList<>();
+        resourcesList.add(resources);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        resources = null;
+    }
+
+    @Test
+    void givenResourceToSaveThenShouldReturnSavedResource() throws Exception {
+        when(resourceService.saveResource(any())).thenReturn(resources);
+        mockMvc.perform(post("/medicine/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(resources)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+        verify(resourceService).saveResource(any());
+    }
+
+    @Test
+    public void givenGetAllResourcesThenShouldReturnListOfAllResources() throws Exception {
+        when(resourceService.getAllResources()).thenReturn(resourcesList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/medicine/getAll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(resources)))
+                .andDo(MockMvcResultHandlers.print());
+        verify(resourceService).getAllResources();
+        verify(resourceService, times(1)).getAllResources();
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
