9ed126a3314f2d1cfde03f67d955406b8ce56575
Shubham Raj <rajs93222@gmail.com>
2021-10-19 15:31:26 +0530

    Taking pull

--- product-webapp/webapp/src/app/components/landing-page/latest-information/latest-information.component.ts
+++ product-webapp/webapp/src/app/components/landing-page/latest-information/latest-information.component.ts
@@ -9,18 +9,24 @@ import {MatPaginator} from '@angular/material/paginator';
   styleUrls: ['./latest-information.component.css']
 })
 export class LatestInformationComponent implements OnInit, AfterViewInit {
+  /**This is used for using angular material paginator to display table in
+   * compact form
+  */
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   displayedColumns: string[] = ['Location', 'Confirmed Cases (Indian)', 'Confirmed Cases (Foreign)', 'Discharged', 'Deaths', 'Total Confirmed'];
   public locationStat = new MatTableDataSource<any>([]);
 
   constructor(private InfoService: InformationService) { }
 
+  /**This method is called when page loads which gets information about current 
+   * cases from backend which in turn calls an external api
+  */
   ngOnInit(): void {
     this.InfoService.getInformation().subscribe(data => {
       console.log(this.locationStat.data = data);
     });
   }
-
+  /**After the page loads the angular material is rendered */
   ngAfterViewInit() {
     this.locationStat.paginator = this.paginator;
   }
