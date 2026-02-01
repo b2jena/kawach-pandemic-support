fa2f4847e718ef228e35bcecf777ae995ea3b662
392574 <bikash.jena@delta.com>
2026-02-01 20:26:14 +0530

    refactor: remove deprecated @EnableEurekaClient and format code
    - Remove @EnableEurekaClient annotation (deprecated in Spring Cloud 2023.x)
    - Service discovery now uses auto-configuration
    - Apply Google Java Format to all application classes
    - Fix indentation and code style issues

--- product-webapp/src/main/java/com/stackroute/productwebapp/ProductWebappApplication.java
+++ product-webapp/src/main/java/com/stackroute/productwebapp/ProductWebappApplication.java
@@ -2,14 +2,11 @@ package com.stackroute.productwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ProductWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductWebappApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProductWebappApplication.class, args);
+  }
 }
